329423751341bb8d8c49efb887a568e34dec5338
TfT_02 <tft_02@hotmail.com>
2014-04-06 16:25:56 +0200

    Format the extract enchant chances

--- src/main/java/com/gmail/nossr50/commands/skills/SalvageCommand.java
+++ src/main/java/com/gmail/nossr50/commands/skills/SalvageCommand.java
@@ -63,14 +63,14 @@ public class SalvageCommand extends SkillCommand {
         if (canArcaneSalvage) {
             SalvageManager salvageManager = UserManager.getPlayer(player).getSalvageManager();
 
-            messages.add(LocaleLoader.getString("Salvage.Arcane.Rank", salvageManager.getArcaneSalvageRank(), Salvage.Tier.EIGHT.toNumerical()));
+            messages.add(LocaleLoader.getString("Salvage.Arcane.Rank", salvageManager.getArcaneSalvageRank(), Salvage.Tier.values().length));
 
             if (Salvage.arcaneSalvageEnchantLoss) {
-                messages.add(LocaleLoader.getString("Ability.Generic.Template", LocaleLoader.getString("Salvage.Arcane.ExtractFull"), salvageManager.getExtractFullEnchantChance()));
+                messages.add(LocaleLoader.getString("Ability.Generic.Template", LocaleLoader.getString("Salvage.Arcane.ExtractFull"), percent.format(salvageManager.getExtractFullEnchantChance() / 100)));
             }
 
             if (Salvage.arcaneSalvageDowngrades) {
-                messages.add(LocaleLoader.getString("Ability.Generic.Template", LocaleLoader.getString("Salvage.Arcane.ExtractPartial"), salvageManager.getExtractPartialEnchantChance()));
+                messages.add(LocaleLoader.getString("Ability.Generic.Template", LocaleLoader.getString("Salvage.Arcane.ExtractPartial"), percent.format(salvageManager.getExtractPartialEnchantChance() / 100)));
             }
         }
 
